c9e6fbb277bf2ab504515543c05ff599fb2ef023
Emeric Espiritu <emericespiritusantiago@gmail.com>
2024-10-07 22:43:58 -0500

    Initial Commit

---
+++ ChatAIOS/ChatAIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAIOSApp.swift
+//  ChatAIOS
+//
+//  Created by Emeric on 7/10/24.
+//
+
+import SwiftUI
+
+@main
+struct ChatAIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
